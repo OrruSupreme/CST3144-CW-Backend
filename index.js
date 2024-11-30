3bbef1d586aef268de0290ffa9496608e45976d6
suprru <supremeorru1@gmail.com>
2024-11-30 18:41:27 +0400

    Removed dependencies

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const reqLogger = (req, res, next) => {
 app.use(reqLogger)
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Server started at port 3000')
 })
 
